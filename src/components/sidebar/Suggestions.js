82881eb23e46bee30bd4db3afc431bad63d5f9c5
Aleksandar Ristić <aristic665@gmail.com>
2021-12-18 00:35:26 +0100

    add: Suggestions component

--- src/components/sidebar/Suggestions.js
+++ src/components/sidebar/Suggestions.js
@@ -1,5 +1,28 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
+import Skeleton from 'react-loading-skeleton'
+import { getSuggestedProfiles } from '../../services/firebase'
 
-export default function Suggestions() {
-	return <div>These are suggestions.</div>
+export default function Suggestions({ userId }) {
+	const [profiles, setProfiles] = useState(null)
+
+	useEffect(() => {
+		async function suggestedProfiles() {
+			const response = await getSuggestedProfiles(userId)
+			setProfiles(response)
+		}
+	}, [userId])
+
+	return !profiles ? (
+		<Skeleton count={1} height={150} className='mt-5' />
+	) : profiles.length > 0 ? (
+		<div className='rounded flex flex-col'>
+			<div className='mb-2 text-sm flex items-center align-items justify-between'></div>
+			<p className='font-bold text-gray-base'>Suggestions for you</p>
+		</div>
+	) : null
+}
+
+Suggestions.propTypes = {
+	userId: PropTypes.string
 }
