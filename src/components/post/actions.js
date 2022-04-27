41e9096ccb05c5e11cda053e626de9b09119229d
Aleksandar Ristić <aristic665@gmail.com>
2022-04-27 13:36:40 +0200

    add: post / actions fill

--- src/components/post/actions.js
+++ src/components/post/actions.js
@@ -1,1 +1,39 @@
+import { useState, useContext } from 'react'
+import propTypes, { arrayOf } from 'prop-types'
+import FirebaseContext from '../../context/firebase'
+import UserContext from '../../context/user'
+import { arrayRemove, arrayUnion } from 'firebase/firestore'
 
+export default function Actions({
+	docId,
+	totalLikes,
+	likedPhoto,
+	handleFocus
+}) {
+	const {
+		user: { uid: userId }
+	} = useContext(UserContext)
+	const [toggleLiked, setToggleLiked] = useState(likedPhoto)
+	const [likes, setLikes] = useState(totalLikes)
+	const { firebase } = useContext(FirebaseContext)
+
+	const handleToggleLiked = async () => {
+		setToggleLiked(toggleLiked => !toggleLiked)
+
+		// await firebase
+		// 	.firestore()
+		// 	.collection('photos')
+		// 	.doc(docId)
+		// 	.update({ likes: toggleLiked ? arrayRemove(userId) : arrayUnion(userId) })
+
+		setLikes(likes => (toggleLiked ? likes - 1 : likes + 1))
+	}
+
+	return (
+		<>
+			<div className='flex justify-between p-4'>
+				<div className='flex'></div>
+			</div>
+		</>
+	)
+}
